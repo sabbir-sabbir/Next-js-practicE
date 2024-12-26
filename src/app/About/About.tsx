e1b37b23bc38f8a47db60e4e2d9be8f4719e4314
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-26 20:24:42 +0600

    Add About page and update Home component structure

---
+++ src/app/About/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div><h1 className='text-center'>This is about page</h1></div>
+    </div>
+  )
+}
+
+export default About
