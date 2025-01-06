e5e5a39fc087f05ca817d6fcd15ce2519d84f115
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-06 20:30:54 +0600

    update

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,8 +1,22 @@
-export default async function ProductDetails({
-    params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
     params: Promise<{productid: string}>
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).productid; 
+    const title =  await new Promise((resolve) => {
+        setTimeout(()=> {
+            resolve(`Iphone ${id}`);
+        }, 100)
+    });
+    return {
+        title: `Product ${title}`, 
+    };
+};
+
+export default async function ProductDetails({ params,}: Props) {
     const productid = (await params).productid;
     return <div>
         <h1>Details About Product:...{productid}</h1>
