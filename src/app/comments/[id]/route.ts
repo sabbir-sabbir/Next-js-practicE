7e62db0599abbcb70e4caaaf5b5f9e4d5066bb7c
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-17 22:57:09 +0600

    dynamic route handling from API

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,8 @@
+import { request } from "http";
+import { comments } from "../data";
+export async function GET(_request: Request, { params }: {params: Promise<{ id: string }> })
+ {
+   const { id } = await params 
+   const comment = comments.find((comment) => comment.id === parseInt(id));
+   return Response.json(comment)
+}
