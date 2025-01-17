fee26a2ad7eef60efe5caeaa67e71280e2089b0b
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-17 22:33:24 +0600

    done get request

---
+++ src/app/comments/data.ts
@@ -0,0 +1,22 @@
+export const comments = [
+    {
+    id: 1,
+    text: 'This is a great post!'
+    },
+    
+    {
+    id: 2,
+    text: 'This is a great post!'
+    },
+
+    {
+    id: 3,
+    text: 'This is a great post!'
+    },
+
+    {
+    id: 4,
+    text: 'This is a great post!'
+    },
+   
+];
