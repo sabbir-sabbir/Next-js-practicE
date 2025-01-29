63393317522d5a2d92abc4d38a96f43bf6b1cbf5
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-29 21:01:39 +0600

    updates

--- src/app/server-route/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ServerRoutePage() {
-    return  <>
-    <div><h1>This is a aerver route !</h1></div>
-    </>
-}
--- src/utils/server-utils.ts
+++
@@ -1,4 +0,0 @@
-export const serverSideFunc = ()=> {
-    console.log('serverSideFunc');
-    return "server results";
-}
