b612bc19c36da34893152faa926c611a28c1a4d3
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-25 20:32:52 +0600

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-
 export default function Home() {
   return (
-   <>
-   <div><h1 className="text-center">Helllo world</h1></div>
-
-   
-   </>
-  );
-}
+    <>
+    <div className="w-full h-auto p-5">
+      <div className="p-2 bg-orange-300 opacity-75 rounded-md"><h1 className=" text-[18px] md:text-2xl lg:text-8xl text-center text-zinc-600">HERE WE GO AGAIN</h1></div>
+    </div>
+    </>
+  )
+};
