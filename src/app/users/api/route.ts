f1311597fd66ebc620188b73956139d002d5efb1
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-26 23:41:59 +0600

    add

--- src/app/users/api/route.ts
+++ src/app/users/api/route.ts
@@ -4,6 +4,28 @@ export async function GET(){
 }
 
 
-export async function POST(){
-    return Response.json(users)
+// POST request
+export async function POST(request: Request){
+    const user = await request.json() 
+
+    const newUser = {
+        id: users.length + 1,
+        name: user.name,
+        email: user.email,
+        age: user.age
+    }
+    users.push(newUser)
+    
+    const resData = {
+        message: 'User created successfully',
+        user: newUser
+    }
+
+    return new Response(JSON.stringify(newUser), {
+        status: 201,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
 }
