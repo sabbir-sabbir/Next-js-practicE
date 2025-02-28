2d3dfc7f020c784f2c0cc3ab5892d94b1dbe4c63
sabbir-sabbir <iosabbir7890@gmail.com>
2025-02-28 23:31:43 +0600

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
     <div className="w-full h-auto p-5">
-      <div className="p-2 bg-orange-300 opacity-75 rounded-md"><h1 className=" text-[18px] md:text-2xl lg:text-8xl text-center text-zinc-600">HERE WE GO AGAIN</h1></div>
+      <div className="p-2 bg-orange-300 opacity-75 rounded-md"><h1 className=" text-[18px] md:text-2xl lg:text-8xl text-center text-zinc-600">THE FUCKING NEXT</h1></div>
     </div>
     </>
   )
